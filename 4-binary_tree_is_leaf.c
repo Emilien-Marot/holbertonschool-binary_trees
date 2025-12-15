a691677a0211df103eb2134b7b207e3b11c7c1da
Emilien-Marot <12394@holbertonstudents.com>
2025-12-15 05:26:35 +0000

    tree 4.0

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - blabla
+ *
+ * @node: abc
+ *
+ * Return: xyz
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+		return (0);
+	if (node->left != NULL)
+		return (0);
+	if (node->right != NULL)
+		return (0);
+	return (1);
+}
